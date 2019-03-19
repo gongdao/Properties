67301088e0fbcc287ccbbbd36cc63b3c68e1b2b3
gongdao <buzhaobin@gmail.com>
2019-03-19 17:27:56 -0400

    add role field to user document

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -28,10 +28,11 @@ export class AuthService {
 
   createUser(email: string, password: string) {
     const authData: AuthData = {email: email, password: password };
+    console.log(authData);
     this .http
       .post('http://localhost:3000/api/user/signup', authData)
       .subscribe(response => {
-        // console.log(response);
+         console.log(response);
       });
   }
 
